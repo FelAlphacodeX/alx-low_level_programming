4d38dc3adbbe972529e65abd6f434213f14992c4
FelAlphacodeX <felcodingx@gmail.com>
2022-10-17 15:18:15 -0700

    diff comb of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,31 +1,36 @@
-#include <studio>
+#include <stdio.h>
 
 /**
  * main - prints all possible different combinations of three digits
  * Return: Always 0
  */
-int main (void) {
-	    char h = '0', t = '1', u = '2';
-	        while ((h <= '7') || (t <= '8') || (u <= '9')) {
-			        printf ("%c%c%c, ", h, t, u);
-				        u != '9' ? ++u : (t != '8' ? (++t, u = t + 1) : (++h, t = h + 1, u = t + 1));
-					    }
-		    return 0;
-}#include <studio>
-
-/**
- * main - prints all possible different combinations of three digits
- * Return: Always 0
- */
-int main (void)
+int main(void)
 {
-	char h = '0', t = '1', u = '2';
+	int ones = '0';
+	int tens = '0';
+	int hundreds = '0';
 
-	while ((h <= '7') || (t <= '8') || (u <= '9'))
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
-		printf("%c%c%c," h,t,u);
-		u != '9'? ++u:(t != '8'?(++t,u = t + 1):(++h, t = h + 1, u = t + 1));
+		for (tens = '0'; tens <= '9' tens++)
+		{
+			for (ones = '0'; ones <= '9'; ones++)
+			{
+				if (!((ones == tens) || (tens == hundreds) ||
+							(tens > ones) || (hundresds > tens)))
+				{
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '9' && hundreds == '7' && tens == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
 	}
-
+	putchar('\n');
 	return (0);
 }
