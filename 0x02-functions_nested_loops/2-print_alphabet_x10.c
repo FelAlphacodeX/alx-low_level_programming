3b2e9c6ca26e06c014bcd2dd0b39bc6ed026ca8e
FelAlphacodeX <felcodingx@gmail.com>
2022-10-18 15:48:01 -0700

    10x alphabets in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+$include "main.c"
+
+/**
+ * print_alphabet_x10 - prints alpha 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char na;
+
+	for (ten = 0; tens <= 9; ten++)
+	{
+		for (na = 'a'; na <= 'z'; na++)
+			_putchar(na);
+
+		_putchar('\n');
+	}
+}
