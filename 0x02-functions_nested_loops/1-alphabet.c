943ea05bb14d15f1a7b47f83bbedd5ca12f0d651
FelAlphacodeX <felcodingx@gmail.com>
2022-10-18 15:42:00 -0700

    prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char na;
+
+	for (na = 'a'; na <= 'z'; na++)
+		_putchar(na);
+
+	_putchar('\n');
+}
