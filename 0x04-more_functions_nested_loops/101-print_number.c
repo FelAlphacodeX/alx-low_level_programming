88939ec30e39f1361eecd99f7b5c8c958ec58525
FelAlphacodeX <felcodingx@gmail.com>
2022-10-21 16:22:49 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,16 @@
  */
 void print_number(int n)
 {
-	unsigned int d = n;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-	_putchar('_');
-	d = -d;
+		_putchar('-');
+		num = -num;
 	}
-	if ((d / 10) > 0)
-	{
-	print_number(d / 10);
-	}
-	_putchar((d % 10) + '0')
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
