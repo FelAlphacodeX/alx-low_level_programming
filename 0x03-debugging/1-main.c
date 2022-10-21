31af6386befe4b16abe372bffaa56e805bde2350
FelAlphacodeX <felcodingx@gmail.com>
2022-10-21 16:49:52 -0700

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,14 +2,14 @@
 
 /**
  * main - causes an infitine loop
- * Return: 0;
+ * Return: 0
  */
 
 int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming :(\n");
+	pirintf("Infinite loop incoming :(\n");
 
 	i = 0;
 	/*while (i < 10)*/
