aa97815b6ca566d8f924e5a57a12f3536291e73d
FelAlphacodeX <felcodingx@gmail.com>
2022-10-27 11:19:20 -0700

    encodes using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -17,14 +17,14 @@ char *rot13(char *str)
 			     'k', 'l', 'm', 'n', 'o', 'p',
 			     'q', 'r', 's', 't', 'u', 'v',
 			     'w', 'x', 'y', 'z'};
-	char rot13ley[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 			     'T', 'U', 'V', 'W', 'X', 'Y',
 			     'Z', 'A', 'B', 'C', 'D', 'E',
 			     'F', 'G', 'H', 'I', 'J', 'K',
 			     'L', 'M', 'n', 'o', 'p', 'q',
 			     'd', 'e', 'f', 'g', 'h', 'i',
 			     'j', 'k', 'l', 'm'};
-	while (str[index1])
+	while (str[indx1])
 	{
 		for (indx2 = 0; indx2 < 52; indx2++)
 		{
